fbb95d531ddf0af5360e7249fb09bd214aad3275
Witalij Kojro <122967296+Witalij-Kojro@users.noreply.github.com>
2024-06-22 07:55:38 +0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,17 +20,17 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+   Check out
+    <a href="/" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
+    <a href="/" target="_blank">Портфолио фото работ.</a>
+    Укадка курамогранита по диоганали.
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">Укладка кафеьной плитки.</p>
 </template>
 
 <style scoped>
