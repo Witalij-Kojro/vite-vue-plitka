adef1326f13236e5368f8d0bb270cac4f8646a96
Witalij Kojro <122967296+Witalij-Kojro@users.noreply.github.com>
2024-06-23 02:21:24 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,43 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div style="position: relative">
+<base-nav type="default" effect="dark" expand>
+    <a class="navbar-brand" href="#">Default Color</a>
+
+    <div class="row" slot="content-header" slot-scope="{closeMenu}">
+        <div class="col-6 collapse-brand">
+            <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
+                <img src="https://demos.creative-tim.com/vue-argon-design-system/img/brand/blue.png">
+            </a>
+        </div>
+        <div class="col-6 collapse-close">
+            <close-button @click="closeMenu"></close-button>
+        </div>
+    </div>
+
+    <ul class="navbar-nav ml-lg-auto">
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                Discover
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                Profile
+            </a>
+        </li>
+        <base-dropdown tag="li" title="Settings">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </base-dropdown>
+    </ul>
+ </base-nav>
+</div>
+
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
