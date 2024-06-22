708f00d400e3416ca1814fed123aa8f8b295efb2
Witalij Kojro <122967296+Witalij-Kojro@users.noreply.github.com>
2024-06-22 07:59:36 +0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,22 +15,22 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> Часный мастер без посредников.
     </p>
   </div>
 
   <p>
-   Check out
+   Прайс:
     <a href="/" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+      >Цены</a
+    >, Цены на укладку керамогранита и керамической плитки.
   </p>
   <p>
-    Install
+    Портфолио:
     <a href="/" target="_blank">Портфолио фото работ.</a>
     Укадка курамогранита по диоганали.
   </p>
-  <p class="read-the-docs">Укладка кафеьной плитки.</p>
+  <p class="read-the-docs">Укладка кафельной плитки.</p>
 </template>
 
 <style scoped>
