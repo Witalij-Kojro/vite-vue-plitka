30f799b707398a35fdfc9197c53d711e393ad29e
Witalij Kojro <122967296+Witalij-Kojro@users.noreply.github.com>
2024-06-23 02:46:51 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,56 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <MDBNavbar expand="lg" light bg="light" container>
-    <MDBNavbarBrand href="#">Brand</MDBNavbarBrand>
-    <MDBNavbarToggler
-      @click="collapse1 = !collapse1"
-      target="#navbarSupportedContent"
-    ></MDBNavbarToggler>
-    <MDBCollapse v-model="collapse1" id="navbarSupportedContent">
-      <MDBNavbarNav class="mb-2 mb-lg-0">
-        <MDBNavbarItem to="#" active>
-          Home
-        </MDBNavbarItem>
-        <MDBNavbarItem href="#">
-          Link
-        </MDBNavbarItem>
-        <MDBNavbarItem>
-          <!-- Navbar dropdown -->
-          <MDBDropdown class="nav-item" v-model="dropdown1">
-            <MDBDropdownToggle
-              tag="a"
-              class="nav-link"
-              @click="dropdown1 = !dropdown1"
-              >Dropdown</MDBDropdownToggle
-            >
-            <MDBDropdownMenu aria-labelledby="dropdownMenuButton">
-              <MDBDropdownItem href="#">Action</MDBDropdownItem>
-              <MDBDropdownItem href="#">Another Action</MDBDropdownItem>
-              <MDBDropdownItem href="#"
-                >Something else here</MDBDropdownItem
-              >
-            </MDBDropdownMenu>
-          </MDBDropdown>
-        </MDBNavbarItem>
-        <MDBNavbarItem to="#" disabled>
-          Disabled
-        </MDBNavbarItem>
-      </MDBNavbarNav>
-      <!-- Search form -->
-      <form class="d-flex input-group w-auto">
-        <input
-          type="search"
-          class="form-control"
-          placeholder="Type query"
-          aria-label="Search"
-        />
-        <MDBBtn outline="primary">
-          Search
-        </MDBBtn>
-      </form>
-    </MDBCollapse>
-  </MDBNavbar>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
